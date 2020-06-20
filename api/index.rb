0b4185b55a740be4937fecba5003d4373f757435
Alex <alexrudall@users.noreply.github.com>
2020-06-21 00:21:10 +0100

    Refactor

--- api/index.rb
+++ api/index.rb
@@ -3,21 +3,21 @@
 require 'net/http'
 require 'json'
 
-Handler = proc do |req, res|
-  if valid(req)
-    from = get_bearings(req.query['from'])
-    to = get_bearings(req.query['to'])
+Handler = proc do |request, response|
+  if valid?(request)
+    from = get_bearings(request.query['from'])
+    to = get_bearings(request.query['to'])
 
     if from.nil?
-      respond(res, 404, "The 'from' postcode could not be found")
+      respond(response, 404, "The 'from' postcode could not be found")
     elsif to.nil?
-      respond(res, 404, "The 'to' postcode could not be found")
+      respond(response, 404, "The 'to' postcode could not be found")
     else
       distance = calculate_distance(from, to).round.to_s
-      respond(res, 200, distance)
+      respond(response, 200, distance)
     end
   else
-    respond(res, 400, "Please provide both a 'from' and a 'to' postcode")
+    respond(response, 400, "Please provide both a 'from' and a 'to' postcode")
   end
 end
 
@@ -35,12 +35,12 @@ def calculate_distance(from, to)
   Math.sqrt(easting_squared + northing_squared)
 end
 
-def valid(req)
-  req.query['from'] && !req.query['from'].empty? && req.query['to'] && !req.query['to'].empty?
+def valid?(request)
+  request.query['from'] && !request.query['from'].empty? && request.query['to'] && !request.query['to'].empty?
 end
 
-def respond(res, status, body)
-  res.status = status
-  res.body = body
-  res['Content-Type'] = 'text/text; charset=utf-8'
+def respond(response, status, body)
+  response.status = status
+  response.body = body
+  response['Content-Type'] = 'text/text; charset=utf-8'
 end
