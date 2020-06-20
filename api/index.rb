ee879f43ae39aa2cf61efe1b8c51bfe2be24e151
Alex <alexrudall@users.noreply.github.com>
2020-06-20 03:08:35 +0100

    Add calculation using Pythagoras's theorem

--- api/index.rb
+++ api/index.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 require 'net/http'
 require 'json'
 
@@ -8,12 +9,18 @@ Handler = Proc.new do |req, res|
 
   res.status = 200
   res['Content-Type'] = 'text/text; charset=utf-8'
-  res.body = '2'
+  res.body = calculate_distance(from, to).round.to_s
 end
 
 def get_bearings(postcode)
   r = Net::HTTP.get('api.getthedata.com', "/postcode/#{CGI.escape(postcode)}")
   r = JSON.parse(r)
   r = r['data']
-  [r['easting'], r['northing']]
+  { easting: r['easting'], northing: r['northing'] }
+end
+
+def calculate_distance(from, to)
+  easting_squared = (from[:easting] - to[:easting]).abs**2
+  northing_squared = (from[:northing] - to[:northing]).abs**2
+  Math.sqrt(easting_squared + northing_squared)
 end
