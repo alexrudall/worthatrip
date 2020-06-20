1b029d223ab79be6116fae72c3d080d49e4b8312
Alex <alexrudall@users.noreply.github.com>
2020-06-21 00:03:25 +0100

    Add error specs and start to add error handling

--- api/index.rb
+++ api/index.rb
@@ -4,12 +4,16 @@ require 'net/http'
 require 'json'
 
 Handler = Proc.new do |req, res|
-  from = get_bearings(req.query['from'])
-  to = get_bearings(req.query['to'])
+  if valid(req)
+    from = get_bearings(req.query['from'])
+    to = get_bearings(req.query['to'])
 
-  res.status = 200
-  res['Content-Type'] = 'text/text; charset=utf-8'
-  res.body = calculate_distance(from, to).round.to_s
+    res.status = 200
+    res['Content-Type'] = 'text/text; charset=utf-8'
+    res.body = calculate_distance(from, to).round.to_s
+  else
+    bad_request(res)
+  end
 end
 
 def get_bearings(postcode)
@@ -23,4 +27,14 @@ def calculate_distance(from, to)
   easting_squared = (from[:easting] - to[:easting]).abs**2
   northing_squared = (from[:northing] - to[:northing]).abs**2
   Math.sqrt(easting_squared + northing_squared)
+end
+
+def valid(req)
+  req.query['from'] && !req.query['from'].empty? && req.query['to'] && !req.query['to'].empty?
+end
+
+def bad_request(res)
+  res.status = 400
+  res['Content-Type'] = 'text/text; charset=utf-8'
+  res.body = "Please provide both a 'from' and a 'to' postcode"
 end
--- spec/index_spec.rb
+++ spec/index_spec.rb
@@ -4,7 +4,7 @@ require 'webrick'
 require '../api/index'
 
 RSpec.describe Handler do
-  context 'with a from and a to postcode' do
+  describe 'GET request' do
     let(:req) do
       r = WEBrick::HTTPRequest.new({})
       r.instance_variable_set(:@query, query)
@@ -12,15 +12,41 @@ RSpec.describe Handler do
     end
     let(:query) {
       {
-        'from' => 'EC3N 4AB',
-        'to' => 'M1 1AG'
+        'from' => from,
+        'to' => to
       }
     }
     let(:res) { WEBrick::HTTPResponse.new(HTTPVersion: WEBrick::HTTPVersion.new('1.1')) }
+    let(:from) { 'EC3N 4AB' }
+    let(:to) { 'M1 1AG' }
 
     it 'responds with the distance' do
-      # https://ukpostcodes.tenfourzero.net/?from=ec3n4ab&to=m11ag gives 263950, accurate to within 10 metres.
+      # Sense checked with https://ukpostcodes.tenfourzero.net/?from=ec3n4ab&to=m11ag
+      # which gives 263950, accurate to within 10 metres.
       expect(Handler.call(req, res)).to eq('263947')
     end
+
+    context 'missing a postcode' do
+      let(:from) { '' }
+
+      it 'responds with an error' do
+        expect(Handler.call(req, res)).to eq("Please provide both a 'from' and a 'to' postcode")
+      end
+    end
+
+    context 'with an invalid postcode' do
+      let(:to) { 'M1' }
+
+      it 'responds with an error' do
+
+      end
+    end
+
+    context 'with a downcased postcode' do
+      it 'responds with the distance' do
+
+      end
+    end
+
   end
 end
