5d303b2e64fe938e64dce0c4dd7529dc7548d338
Alex <alexrudall@users.noreply.github.com>
2020-06-21 09:14:24 +0100

    Add VCR and cache calls to API in specs

--- Gemfile
+++ Gemfile
@@ -2,13 +2,23 @@
 
 source 'https://rubygems.org'
 
+# HTTP server toolkit.
+# https://github.com/ruby/webrick
+gem 'webrick'
+
+# Testing #############
+
 # Behaviour Driven Development for Ruby.
 # https://github.com/rspec/rspec
 gem 'rspec', '~> 3.9.0'
 
-# HTTP server toolkit.
-# https://github.com/ruby/webrick
-gem 'webrick'
+# Cache results of externa HTTP requests.
+# https://github.com/vcr/vcr
+gem 'vcr', '~> 6.0.0'
+
+# Stub and set expectations on HTTP requests.
+# https://github.com/bblimke/webmock
+gem 'webmock', '~> 3.8.3'
 
 # Temp.
 gem 'byebug'
--- spec/api/v1/distance_spec.rb
+++ spec/api/v1/distance_spec.rb
@@ -2,6 +2,8 @@
 
 require 'webrick'
 require '../api/v1/distance'
+require 'vcr'
+require_relative '../../support/vcr'
 
 RSpec.describe Handler do
   describe 'GET request' do
@@ -20,17 +22,27 @@ RSpec.describe Handler do
     let(:from) { 'EC3N 4AB' }
     let(:to) { 'M1 1AG' }
 
-    before { Handler.call(request, response) }
+    context 'with valid postcodes' do
+      before do
+        VCR.use_cassette('get_distance') do
+          Handler.call(request, response)
+        end
+      end
 
-    it 'responds with the distance' do
-      # Sense checked with https://ukpostcodes.tenfourzero.net/?from=ec3n4ab&to=m11ag
-      # which gives 263950, accurate to within 10 metresponse.
-      expect(response.status).to eq(200)
-      expect(response.body).to eq('263947')
+      it 'responds with the distance' do
+        # Sense checked with https://ukpostcodes.tenfourzero.net/?from=ec3n4ab&to=m11ag
+        # which gives 263950, accurate to within 10 metresponse.
+        expect(response.status).to eq(200)
+        expect(response.body).to eq('263947')
+      end
     end
 
-    context 'missing a postcode' do
+    context 'when missing a postcode' do
       let(:from) { '' }
+
+      before do
+        Handler.call(request, response)
+      end
 
       it 'responds with a 400 error' do
         expect(response.status).to eq(400)
@@ -41,6 +53,12 @@ RSpec.describe Handler do
     context 'with an invalid origin postcode' do
       let(:from) { '$%^' }
 
+      before do
+        VCR.use_cassette('get_distance_bad_from') do
+          Handler.call(request, response)
+        end
+      end
+
       it 'responds with a 404 error' do
         expect(response.status).to eq(404)
         expect(response.body).to eq("The 'from' postcode could not be found")
@@ -50,6 +68,12 @@ RSpec.describe Handler do
     context 'with an invalid destination postcode' do
       let(:to) { '%^&' }
 
+      before do
+        VCR.use_cassette('get_distance_bad_to') do
+          Handler.call(request, response)
+        end
+      end
+
       it 'responds with a 404 error' do
         expect(response.status).to eq(404)
         expect(response.body).to eq("The 'to' postcode could not be found")
@@ -58,6 +82,12 @@ RSpec.describe Handler do
 
     context 'with a downcased postcode' do
       let(:from) { 'ec3n 4ab' }
+
+      before do
+        VCR.use_cassette('get_distance_downcase') do
+          Handler.call(request, response)
+        end
+      end
 
       it 'responds with the distance' do
         expect(response.status).to eq(200)
