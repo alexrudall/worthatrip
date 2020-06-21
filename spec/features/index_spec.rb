945b694d196feaabd911eb29c3721a5dbfb7be42
Alex <alexrudall@users.noreply.github.com>
2020-06-22 00:50:09 +0100

    Add content

--- spec/features/index_spec.rb
+++ spec/features/index_spec.rb
@@ -3,8 +3,12 @@ require 'rack/file'
 Capybara.app = Rack::File.new File.dirname '.'
 
 describe 'index', type: :feature do
-  it 'contains Hello World!' do
+  it 'contains content' do
     visit 'index.html'
-    expect(page).to have_content 'Hello world!'
+    expect(page).to have_content 'Where are you?'
+    expect(page).to have_content 'How far will you go?'
+    expect(page).to have_content 'Where is the requested dropoff?'
+    expect(page).to have_content "You don't deliver this far!"
+    expect(page).to have_content 'Nice, you deliver to this address!'
   end
 end
