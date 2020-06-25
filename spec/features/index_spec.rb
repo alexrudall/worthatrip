bfdc3947fb1a7bdaedd5b9fe28f749ffe58b70af
Alex <alexrudall@users.noreply.github.com>
2020-06-25 19:58:29 +0100

    Refactor specs

--- spec/features/index_spec.rb
+++ spec/features/index_spec.rb
@@ -5,19 +5,26 @@ Capybara.app = Rack::File.new File.dirname '.'
 describe 'index', type: :feature do
   before { visit 'index.html' }
 
-  it 'contains content' do
-    expect(page).to have_content 'Where are you?'
-    expect(page).to have_content 'How far will you go?'
-    expect(page).to have_content 'Where is the dropoff?'
-    expect(page).to have_content "You don't deliver this far!"
-    expect(page).to have_content 'Nice, you deliver to this location!'
-  end
-
   describe 'distance form' do
-    it 'can be filled out' do
-      fill_in 'from', with: 'some text'
-      fill_in 'max', with: '2'
-      fill_in 'to', with: 'some text'
+    let(:from) { 'EC3N 4AB' }
+    let(:to) { 'M1 1AG' }
+
+    context 'when too far' do
+      it 'notifies user' do
+        fill_in 'Where are you?', with: from
+        fill_in 'How far will you go?', with: '2'
+        fill_in 'Where is the dropoff?', with: to
+        expect(page).to have_content "You don't deliver this far!"
+      end
+    end
+
+    context 'when within range' do
+      it 'notifies user' do
+        fill_in 'Where are you?', with: 'some text'
+        fill_in 'How far will you go?', with: '2000'
+        fill_in 'Where is the dropoff?', with: 'some text'
+        expect(page).to have_content 'Nice, you deliver to this location!'
+      end
     end
   end
 end
