9b165b52cadbce280b7fcb44c143954fb8089f8a
Alex <alexrudall@users.noreply.github.com>
2020-06-27 20:26:05 +0100

    Use hidden instead of invisible

--- javascript/distance.js
+++ javascript/distance.js
@@ -1,11 +1,11 @@
 function checkDistance(event) {
   event.preventDefault();
-  document.getElementById('results').classList.remove('invisible');
+  document.getElementById('results').classList.remove('hidden');
   document.getElementById('output').innerHTML = 'Loading...';
   document.getElementById('from').classList.remove('border-red-500');
   document.getElementById('to').classList.remove('border-red-500');
-  document.getElementById('from-error').classList.add('invisible');
-  document.getElementById('to-error').classList.add('invisible');
+  document.getElementById('from-error').classList.add('hidden');
+  document.getElementById('to-error').classList.add('hidden');
 
   var from = event.currentTarget[0].value
   var max = event.currentTarget[1].value
@@ -17,7 +17,7 @@ function checkDistance(event) {
   request.onload = function() {
     if (this.status == 200) {
       var distance = parseInt(request.responseText);
-      document.getElementById('results').classList.remove('invisible');
+      document.getElementById('results').classList.remove('hidden');
 
       if (distance <= max) {
         document.getElementById('output').innerHTML = 'Nice, you deliver to this location!';
@@ -25,15 +25,15 @@ function checkDistance(event) {
         document.getElementById('output').innerHTML = "You don't deliver this far!";
       }
     } else {
-      document.getElementById('results').classList.add('invisible');
+      document.getElementById('results').classList.add('hidden');
 
       if (request.responseText.includes('from')) {
         document.getElementById('from').classList.add('border-red-500');
-        document.getElementById('from-error').classList.remove('invisible');
+        document.getElementById('from-error').classList.remove('hidden');
         document.getElementById('from-error').innerHTML = request.responseText;
       } else if (request.responseText.includes('to')) {
         document.getElementById('to').classList.add('border-red-500');
-        document.getElementById('to-error').classList.remove('invisible');
+        document.getElementById('to-error').classList.remove('hidden');
         document.getElementById('to-error').innerHTML = request.responseText;
       }
     }
