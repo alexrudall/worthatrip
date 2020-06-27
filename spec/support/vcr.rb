4f99bb590daae1b938d6aba3d98d2f759dcbe79a
Alex <alexrudall@users.noreply.github.com>
2020-06-27 15:14:32 +0100

    Get specs passing vs production

--- spec/support/vcr.rb
+++ spec/support/vcr.rb
@@ -1,4 +1,5 @@
 VCR.configure do |config|
-  config.cassette_library_dir = "spec/support/fixtures/vcr_cassettes"
+  config.cassette_library_dir = 'spec/support/fixtures/vcr_cassettes'
   config.hook_into :webmock
+  config.ignore_hosts '127.0.0.1', 'https://isitfar.vercel.app'
 end
