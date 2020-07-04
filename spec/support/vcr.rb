3073e0f40c8b443d749f8ea0fb4cfca3bd86d570
Alex <alexrudall@users.noreply.github.com>
2020-07-04 18:27:26 +0100

    Rename to worththetrip

--- spec/support/vcr.rb
+++ spec/support/vcr.rb
@@ -3,5 +3,5 @@
 VCR.configure do |config|
   config.cassette_library_dir = 'spec/support/fixtures/vcr_cassettes'
   config.hook_into :webmock
-  config.ignore_hosts '127.0.0.1', 'https://isitfar.vercel.app'
+  config.ignore_hosts '127.0.0.1', 'https://worththetrip.vercel.app'
 end
